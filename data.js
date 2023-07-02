1239a5f156fbc4fcec48c4b6a10ac5ddc9508921
xuanviet73 <2012393@dlu.edu.vn>
2023-07-03 00:41:54 +0700

    DaLaTPalaceVR

---
+++ data.js
@@ -0,0 +1,226 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dalat-palace01",
+      "name": "Dalat Palace01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0157706710190944,
+          "pitch": 0.43791405993254884,
+          "rotation": 0,
+          "target": "1-dalat-palace02"
+        },
+        {
+          "yaw": -1.128354212658543,
+          "pitch": 0.4668516891352361,
+          "rotation": 1.5707963267948966,
+          "target": "3-dalat-palace04"
+        },
+        {
+          "yaw": -3.079560328398035,
+          "pitch": 0.8870055966006412,
+          "rotation": 0,
+          "target": "4-dalat-palace05"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.029990568213348,
+          "pitch": 0.051484873120040575,
+          "title": "Khách sạn Dalat Palace",
+          "text": "<div>Khách sạn Dalat Palace là khách sạn duy nhất vẫn còn lưu giữ kiến ​​trúc hoài cổ từ thời Pháp. Được xây dựng vào năm 1916 và gần như đi vào hoạt động vào năm 1922 có tên Lang bian. Sau đổi tên thành khách sạn Dalat Palace Hotel .</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-dalat-palace02",
+      "name": "Dalat Palace02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.008730796871077473,
+          "pitch": -0.152526328245159,
+          "rotation": 0,
+          "target": "2-dalat-palace03"
+        },
+        {
+          "yaw": -3.038415991158196,
+          "pitch": -0.05818231486821546,
+          "rotation": 0,
+          "target": "0-dalat-palace01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dalat-palace03",
+      "name": "Dalat Palace03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9375947969980754,
+          "pitch": -0.3477968132616738,
+          "rotation": 4.71238898038469,
+          "target": "1-dalat-palace02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-dalat-palace04",
+      "name": "Dalat Palace04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1794457683026724,
+          "pitch": -0.1255173515526451,
+          "rotation": 0,
+          "target": "0-dalat-palace01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-dalat-palace05",
+      "name": "Dalat Palace05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8389029583086298,
+          "pitch": -0.024354971501221456,
+          "rotation": 0,
+          "target": "0-dalat-palace01"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "DalatPalace",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
